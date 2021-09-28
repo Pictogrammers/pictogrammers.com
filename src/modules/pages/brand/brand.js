ac9818e0ff09eb6b6d5e5eb591bc5789e8428c15
Michael Irigoyen <michael@irigoyen.dev>
2021-09-27 23:10:20 -0500

    Begin work on brand guidelines page

--- src/modules/pages/brand/brand.js
+++ src/modules/pages/brand/brand.js
@@ -1,4 +1,12 @@
 import { LightningElement } from 'lwc';
+import { mdiArrowDown } from '@mdi/js';
 
 export default class Brand extends LightningElement {
+  iconExportMapping = {
+    mdiArrowDown
+  };
+
+  get icon() {
+    return this.iconExportMapping;
+  }
 }
