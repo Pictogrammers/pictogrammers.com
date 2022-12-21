594dcc040659e676a5b4a0dc7f329a310a30be44
Michael Irigoyen <michael@irigoyen.dev>
2022-12-20 23:38:54 -0600

    Add a couple example links for docs

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
 import Head from 'next/head';
 import Paper from '@mui/material/Paper';
 
@@ -16,6 +17,11 @@ const DocsLandingPage: NextPage = () => {
         <p>Learn how to get started with our icon and font libraries in your project.</p>
         <p>TODO: Design and style this page</p>
         <p>TODO: Show stylized cards for each category: Getting Started, Contributing, Library-specific Guides, etc dynamicly from generated MDX files</p>
+        <p>Examples:</p>
+        <ul>
+          <li><Link href='/docs/mdi/getting-started'>MDI Getting Started</Link></li>
+          <li><Link href='/docs/mdi/guides/home-assistant'>MDI Home Assistant Guide</Link></li>
+        </ul>
       </Paper>
     </div>
   );
